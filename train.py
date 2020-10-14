ba7b364b9e64e3d9908bc01c0566112265cebe0a
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-10-14 09:52:49 +0800

    Update train.py

--- train.py
+++ train.py
@@ -204,6 +204,14 @@ if __name__ == "__main__":
     with open(r"VOCdevkit/VOC2007/ImageSets/Segmentation/val.txt","r") as f:
         val_lines = f.readlines()
         
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Interval_Epoch为冻结训练的世代
+    #   Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
     if True:
         lr = 1e-4
         Init_Epoch = 0
