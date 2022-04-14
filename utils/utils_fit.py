b81a298a64a0becc9b9a9a1b461454df53ff4b2e
Bubbliiiing <3323290568@qq.com>
2022-04-14 22:06:00 +0800

    update multi gpus

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -8,34 +8,72 @@ from utils.utils import get_lr
 from utils.utils_metrics import f_score
 
 
-def fit_one_epoch(model_train, model, loss_history, optimizer, epoch, epoch_step, epoch_step_val, gen, gen_val, Epoch, cuda, dice_loss, focal_loss, cls_weights, aux_branch, num_classes, fp16, scaler, save_period, save_dir):
+def fit_one_epoch(model_train, model, loss_history, optimizer, epoch, epoch_step, epoch_step_val, gen, gen_val, Epoch, cuda, dice_loss, focal_loss, cls_weights, aux_branch, num_classes, \
+    fp16, scaler, save_period, save_dir, local_rank=0):
+    
     total_loss      = 0
     total_f_score   = 0
 
     val_loss        = 0
     val_f_score     = 0
 
+    if local_rank == 0:
+        print('Start Train')
+        pbar = tqdm(total=epoch_step,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
     model_train.train()
-    print('Start Train')
-    with tqdm(total=epoch_step,desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
-        for iteration, batch in enumerate(gen):
-            if iteration >= epoch_step: 
-                break
-            imgs, pngs, labels = batch
+    for iteration, batch in enumerate(gen):
+        if iteration >= epoch_step: 
+            break
+        imgs, pngs, labels = batch
+
+        with torch.no_grad():
+            imgs    = torch.from_numpy(imgs).type(torch.FloatTensor)
+            pngs    = torch.from_numpy(pngs).long()
+            labels  = torch.from_numpy(labels).type(torch.FloatTensor)
+            weights = torch.from_numpy(cls_weights)
+            if cuda:
+                imgs    = imgs.cuda()
+                pngs    = pngs.cuda()
+                labels  = labels.cuda()
+                weights = weights.cuda()
+
+        optimizer.zero_grad()
+        if not fp16:
+            if aux_branch:
+                aux_outputs, outputs = model_train(imgs)
+                if focal_loss:
+                    aux_loss  = Focal_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
+                    main_loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
+                else:
+                    aux_loss  = CE_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
+                    main_loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
+                loss      = aux_loss * 0.4 + main_loss
+                if dice_loss:
+                    aux_dice  = Dice_loss(aux_outputs, labels)
+                    main_dice = Dice_loss(outputs, labels)
+                    loss      = loss + aux_dice * 0.4 + main_dice
+            else:
+                outputs = model_train(imgs)
+                if focal_loss:
+                    loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
+                else:
+                    loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
+
+                if dice_loss:
+                    main_dice = Dice_loss(outputs, labels)
+                    loss      = loss + main_dice
 
             with torch.no_grad():
-                imgs    = torch.from_numpy(imgs).type(torch.FloatTensor)
-                pngs    = torch.from_numpy(pngs).long()
-                labels  = torch.from_numpy(labels).type(torch.FloatTensor)
-                weights = torch.from_numpy(cls_weights)
-                if cuda:
-                    imgs    = imgs.cuda()
-                    pngs    = pngs.cuda()
-                    labels  = labels.cuda()
-                    weights = weights.cuda()
+                #-------------------------------#
+                #   计算f_score
+                #-------------------------------#
+                _f_score = f_score(outputs, labels)
 
-            optimizer.zero_grad()
-            if not fp16:
+            loss.backward()
+            optimizer.step()
+        else:
+            from torch.cuda.amp import autocast
+            with autocast():
                 if aux_branch:
                     aux_outputs, outputs = model_train(imgs)
                     if focal_loss:
@@ -66,118 +104,88 @@ def fit_one_epoch(model_train, model, loss_history, optimizer, epoch, epoch_step
                     #-------------------------------#
                     _f_score = f_score(outputs, labels)
 
-                loss.backward()
-                optimizer.step()
-            else:
-                from torch.cuda.amp import autocast
-                with autocast():
-                    if aux_branch:
-                        aux_outputs, outputs = model_train(imgs)
-                        if focal_loss:
-                            aux_loss  = Focal_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
-                            main_loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
-                        else:
-                            aux_loss  = CE_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
-                            main_loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
-                        loss      = aux_loss * 0.4 + main_loss
-                        if dice_loss:
-                            aux_dice  = Dice_loss(aux_outputs, labels)
-                            main_dice = Dice_loss(outputs, labels)
-                            loss      = loss + aux_dice * 0.4 + main_dice
-                    else:
-                        outputs = model_train(imgs)
-                        if focal_loss:
-                            loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
-                        else:
-                            loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
+            #----------------------#
+            #   反向传播
+            #----------------------#
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
-                        if dice_loss:
-                            main_dice = Dice_loss(outputs, labels)
-                            loss      = loss + main_dice
-
-                    with torch.no_grad():
-                        #-------------------------------#
-                        #   计算f_score
-                        #-------------------------------#
-                        _f_score = f_score(outputs, labels)
-
-                #----------------------#
-                #   反向传播
-                #----------------------#
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-
-            total_loss      += loss.item()
-            total_f_score   += _f_score.item()
-            
+        total_loss      += loss.item()
+        total_f_score   += _f_score.item()
+        
+        if local_rank == 0:
             pbar.set_postfix(**{'total_loss': total_loss / (iteration + 1), 
                                 'f_score'   : total_f_score / (iteration + 1),
                                 'lr'        : get_lr(optimizer)})
             pbar.update(1)
 
-    print('Finish Train')
+    if local_rank == 0:
+        pbar.close()
+        print('Finish Train')
+        print('Start Validation')
+        pbar = tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3)
 
     model_train.eval()
-    print('Start Validation')
-    with tqdm(total=epoch_step_val, desc=f'Epoch {epoch + 1}/{Epoch}',postfix=dict,mininterval=0.3) as pbar:
-        for iteration, batch in enumerate(gen_val):
-            if iteration >= epoch_step_val:
-                break
-            imgs, pngs, labels = batch
-            with torch.no_grad():
-                imgs    = torch.from_numpy(imgs).type(torch.FloatTensor)
-                pngs    = torch.from_numpy(pngs).long()
-                labels  = torch.from_numpy(labels).type(torch.FloatTensor)
-                weights = torch.from_numpy(cls_weights)
-                if cuda:
-                    imgs    = imgs.cuda()
-                    pngs    = pngs.cuda()
-                    labels  = labels.cuda()
-                    weights = weights.cuda()
-                #-------------------------------#
-                #   判断是否使用辅助分支
-                #-------------------------------#
-                if aux_branch:
-                    aux_outputs, outputs = model_train(imgs)
-                    if focal_loss:
-                        aux_loss  = Focal_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
-                        main_loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
-                    else:
-                        aux_loss  = CE_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
-                        main_loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
-                    loss      = aux_loss * 0.4 + main_loss
-                    if dice_loss:
-                        aux_dice  = Dice_loss(aux_outputs, labels)
-                        main_dice = Dice_loss(outputs, labels)
-                        loss      = loss + aux_dice * 0.4 + main_dice
+    for iteration, batch in enumerate(gen_val):
+        if iteration >= epoch_step_val:
+            break
+        imgs, pngs, labels = batch
+        with torch.no_grad():
+            imgs    = torch.from_numpy(imgs).type(torch.FloatTensor)
+            pngs    = torch.from_numpy(pngs).long()
+            labels  = torch.from_numpy(labels).type(torch.FloatTensor)
+            weights = torch.from_numpy(cls_weights)
+            if cuda:
+                imgs    = imgs.cuda()
+                pngs    = pngs.cuda()
+                labels  = labels.cuda()
+                weights = weights.cuda()
+            #-------------------------------#
+            #   判断是否使用辅助分支
+            #-------------------------------#
+            if aux_branch:
+                aux_outputs, outputs = model_train(imgs)
+                if focal_loss:
+                    aux_loss  = Focal_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
+                    main_loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
                 else:
-                    outputs = model_train(imgs)
-                    if focal_loss:
-                        loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
-                    else:
-                        loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
+                    aux_loss  = CE_Loss(aux_outputs, pngs, weights, num_classes = num_classes)
+                    main_loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
+                loss      = aux_loss * 0.4 + main_loss
+                if dice_loss:
+                    aux_dice  = Dice_loss(aux_outputs, labels)
+                    main_dice = Dice_loss(outputs, labels)
+                    loss      = loss + aux_dice * 0.4 + main_dice
+            else:
+                outputs = model_train(imgs)
+                if focal_loss:
+                    loss = Focal_Loss(outputs, pngs, weights, num_classes = num_classes)
+                else:
+                    loss = CE_Loss(outputs, pngs, weights, num_classes = num_classes)
 
-                    if dice_loss:
-                        main_dice = Dice_loss(outputs, labels)
-                        loss      = loss + main_dice
-                #-------------------------------#
-                #   计算f_score
-                #-------------------------------#
-                _f_score    = f_score(outputs, labels)
+                if dice_loss:
+                    main_dice = Dice_loss(outputs, labels)
+                    loss      = loss + main_dice
+            #-------------------------------#
+            #   计算f_score
+            #-------------------------------#
+            _f_score    = f_score(outputs, labels)
 
-                val_loss    += loss.item()
-                val_f_score += _f_score.item()
-                
+            val_loss    += loss.item()
+            val_f_score += _f_score.item()
             
+        if local_rank == 0:
             pbar.set_postfix(**{'val_loss'  : val_loss / (iteration + 1),
                                 'f_score'   : val_f_score / (iteration + 1),
                                 'lr'        : get_lr(optimizer)})
             pbar.update(1)
             
-    loss_history.append_loss(epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)
-    print('Finish Validation')
-    print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
-    print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
-    if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
-        torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
+    if local_rank == 0:
+        pbar.close()
+        print('Finish Validation')
+        loss_history.append_loss(epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)
+        print('Epoch:'+ str(epoch + 1) + '/' + str(Epoch))
+        print('Total Loss: %.3f || Val Loss: %.3f ' % (total_loss / epoch_step, val_loss / epoch_step_val))
+        if (epoch + 1) % save_period == 0 or epoch + 1 == Epoch:
+            torch.save(model.state_dict(), os.path.join(save_dir, 'ep%03d-loss%.3f-val_loss%.3f.pth' % (epoch + 1, total_loss / epoch_step, val_loss / epoch_step_val)))
