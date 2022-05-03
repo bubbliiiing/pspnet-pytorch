5301c153629f01686ac29a13c36cbd2883955ee0
Bubbliiiing <3323290568@qq.com>
2022-05-03 12:44:54 +0800

    update comment

--- pspnet.py
+++ pspnet.py
@@ -10,7 +10,7 @@ from PIL import Image
 from torch import nn
 
 from nets.pspnet import PSPNet as pspnet
-from utils.utils import cvtColor, preprocess_input, resize_image
+from utils.utils import cvtColor, preprocess_input, resize_image, show_config
 
 
 #-----------------------------------------------------------------------------------#
@@ -81,6 +81,8 @@ class PSPNet(object):
         #   获得模型
         #---------------------------------------------------#
         self.generate()
+        
+        show_config(**self._defaults)
 
     #---------------------------------------------------#
     #   获得所有的分类
--- summary.py
+++ summary.py
@@ -1,10 +1,30 @@
 #--------------------------------------------#
 #   该部分代码只用于看网络结构，并非测试代码
 #--------------------------------------------#
+import torch
+from thop import clever_format, profile
 from torchsummary import summary
 
 from nets.pspnet import PSPNet
 
 if __name__ == "__main__":
-    model = PSPNet(num_classes=21, backbone="mobilenet", downsample_factor=16, aux_branch=False, pretrained=False).train().cuda()
-    summary(model,(3, 473, 473))
+    input_shape     = [512, 512]
+    num_classes     = 21
+    backbone        = 'mobilenet'
+    
+    device  = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = PSPNet(num_classes=num_classes, backbone=backbone, downsample_factor=16, aux_branch=False, pretrained=False).to(device)
+    summary(model, (3, input_shape[0], input_shape[1]))
+    
+    dummy_input     = torch.randn(1, 3, input_shape[0], input_shape[1]).to(device)
+    flops, params   = profile(model.to(device), (dummy_input, ), verbose=False)
+    #--------------------------------------------------------#
+    #   flops * 2是因为profile没有将卷积作为两个operations
+    #   有些论文将卷积算乘法、加法两个operations。此时乘2
+    #   有些论文只考虑乘法的运算次数，忽略加法。此时不乘2
+    #   本代码选择乘2，参考YOLOX。
+    #--------------------------------------------------------#
+    flops           = flops * 2
+    flops, params   = clever_format([flops, params], "%.3f")
+    print('Total GFLOPS: %s' % (flops))
+    print('Total params: %s' % (params))
