1249664ef6e54b3fda5e3eaf040ac9d8ac15eca6
Bubbliiiing <3323290568@qq.com>
2022-05-05 11:57:05 +0800

    update voc_annotation.py and count

--- pspnet.py
+++ pspnet.py
@@ -105,7 +105,7 @@ class PSPNet(object):
     #---------------------------------------------------#
     #   检测图片
     #---------------------------------------------------#
-    def detect_image(self, image, count=False, name_classes=False):
+    def detect_image(self, image, count=False, name_classes=None):
         #---------------------------------------------------------#
         #   在这里将图像转换成RGB图像，防止灰度图在预测时报错。
         #   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
